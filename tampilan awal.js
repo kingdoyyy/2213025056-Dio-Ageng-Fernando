9e711ea5d7fc2180fca8f4ef582bfafb0ce4b5db
kingdoyyy <dioagengfernando@gmail.com>
2025-06-12 01:33:21 +0700

    Update tampilan awal.js

--- tampilan awal.js
+++ tampilan awal.js
@@ -1,9 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
     const taskButtons = document.querySelectorAll(".task-button");
-    
-    taskButtons.forEach(button => {
+    const task ["Tugas 1","Tugas 2","Tugas 3", "Kuis 1","Kuis 2","Kuis 3","UTS","UAS"];
+    taskButtons.forEach((button, index) => {
         button.addEventListener("click", () => {
-            alert(`Anda mengklik ${button.dataset.task}`);
+            alert(`Anda mengklik ${task[index]}`);
         });
     });
 });
